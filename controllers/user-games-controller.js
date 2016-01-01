a0d785b1c0a1288eabb5658893ec829bccc9b4a0
M-Yankov <pointblank12345@abv.bg>
2016-01-01 16:37:56 +0200

    Bug fixed endless loading on client - forgotten vm.Loaded in UserGamesController

--- controllers/user-games-controller.js
+++ controllers/user-games-controller.js
@@ -5,6 +5,7 @@
     function UsersGamesController(gameManager, notifier) {
 
         var vm = this;
+        vm.isLoaded = false;
 
         getAllGames();
 
@@ -12,8 +13,11 @@
             gameManager.getPrivateGames()
                 .then(function (games) {
                     vm.allGames = games;
+                    vm.isLoaded = true;
                 }, function (errorResponse) {
                     var errors = {};
+                    vm.isLoaded = true;
+
                     notifier.error(errorResponse.data.Message, 'Error');
 
                     if (errorResponse.data && errorResponse.data.ModelState[""]) {
